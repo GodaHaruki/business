d5070d6bd2e8fa2dd5be1c8ea7105ec860f8a221
Okay-Love <haru.11.saki.11@gmail.com>
2024-07-04 00:32:38 +0900

    add Grid component

---
+++ app/container.tsx
@@ -0,0 +1,39 @@
+export const Grid = ({children} : {children : React.ReactNode}) => {
+  return (
+    <div className="grid grid-cols-12 gap-4">
+      {children}
+    </div>
+  )
+}
+
+export const Grid1 = ({children} : {children: React.ReactNode}) => {
+  return (
+    <div className="col-span-12">
+      {children}
+    </div>
+  )
+}
+
+export const Grid2 = ({children} : {children: React.ReactNode}) => {
+  return (
+    <div className="col-span-6">
+      {children}
+    </div>
+  )
+}
+
+export const Grid3 = ({children} : {children: React.ReactNode}) => {
+  return (
+    <div className="col-span-4">
+      {children}
+    </div>
+  )
+}
+
+export const Grid4 = ({children} : {children: React.ReactNode}) => {
+  return (
+    <div className="col-span-3">
+      {children}
+    </div>
+  )
+}
